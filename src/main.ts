765a1b55a003b387ebd0a09c2664c5287d7cbc29
Thomas Boop <thboop@microsoft.com>
2020-01-07 15:38:10 -0500

    Verify Issue works

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,9 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    core.info(`Issue context is: ${JSON.stringify(github.context.issue)}`)
   } catch (error) {
     core.setFailed(error.message)
   }
