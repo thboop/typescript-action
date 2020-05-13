46ac90d2d74d33095a7087de5ab684f4f3df126a
Thomas Boop <thboop@github.com>
2020-05-13 11:10:53 -0400

    test tc changes

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import * as core from '@actions/core'
+import * as tc from '@actions/tool-cache'
+/* eslint-disable no-empty */
 
 async function run(): Promise<void> {
   try {
-    core.exportVariable('LD_LIBRARY_PATH', 'test')
-    core.exportVariable('LA_LIBRARY_PATH', 'test')
-    core.info('Ran the action')
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+    const node12Path = await tc.downloadTool(
+      'https://nodejs.org/dist/v12.7.0/node-v12.7.0.pkg'
+    )
+    await tc.extractXar(node12Path, 'test/test2')
+  } catch {}
 }
 
 run()
